b63114753a2e6d9cd259e2a409b5758bfe367018
igorSobayev <isobayev@gmail.com>
2019-01-14 19:47:05 +0100

    Lógica del juego de index acabada, queda colocar los elementos en su sitio y retocar un poco

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 $( function() {
-    $( ".draggable" ).draggable();
-    $( "#droppable" ).droppable({
+    var draggables = $( ".draggable" ).draggable();
+
+    // Guardo el valor actual del elemento
+    draggables.mousedown(function() {
+        draggable_actual = $(this).attr("id");
+        console.log(draggable_actual);
+    })
+
+    // Compruebo antes de cambiar si es correcta la respuesta
+    $( ".droppable" ).droppable({
       drop: function( event, ui ) {
+        droppable_actual = $(this).attr("id");
+        console.log($(this).attr("id"));
+        switch(draggable_actual) {
+            case droppable_actual:
+                $(this).css("background-image", "url(imagenes/correcto.png)");
+        }/*
+        if (droppable_actual == draggable_actual) {
+            alert("WASUUUUUUU");
+        }*/
+
+        /*
         $( this )
           .addClass( "ui-state-highlight" )
           .find( "#curiosity" )
-            .html( "¡Genial, es Curiosity!" );
+        .html( "¡Genial, es Curiosity!" );*/
       }
     });
   } );
