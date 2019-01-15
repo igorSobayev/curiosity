0d466f0715c946a6701dee219ebe874b37568439
igorSobayev <isobayev@gmail.com>
2019-01-15 12:28:24 +0100

    Terminada la página de pasado, añadido un acordeon con los cinco robots más famosos de la historia y un botón al final de todo que te lleva alsiguiente apartado.

--- script.js
+++ script.js
@@ -35,4 +35,20 @@ $( function() {
     $( "#tabs" ).tabs({
       collapsible: true
     });
+
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      icons: icons,
+      heightStyle: "content"
+    });
+    $( "#toggle" ).button().on( "click", function() {
+      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+        $( "#accordion" ).accordion( "option", "icons", null );
+      } else {
+        $( "#accordion" ).accordion( "option", "icons", icons );
+      }
+    });
   } );
