e1b22265d3670349a50e297e91e31052f126c99f
igorSobayev <isobayev@gmail.com>
2019-01-17 14:12:13 +0100

    Comentarios añadidos al codigo js y al css. Codigo añadido al futuro para que las tarjetas giren al pulsar sobre ellas. Pagina terminada casi por completo solo queda el futuro

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 $(function () {
+
+  /* Codigo JavaScript y jQuery UI
+  Primero guardamos en una variable todos los elementos draggables de la página y les aplicamos
+  dos propiedades, la del containment para que solo pueda estar en su contenedor principal y la de revert
+  que devuelve la imagen seleccionada a su origen. */
   var draggables = $(".draggable").draggable({ containment: ".contenedor-juego", revert: true });
 
-  // Guardo el valor actual del elemento
+  /* Utilizo la variable de antes para darle un evento cuando pulsas sobre una imagen
+  y la mueves, el evento consiste en guardar el id del elemento seleccionado y el elemento total
+  en una variable para luego utilizarlo en el droppable en la comparacion */
   draggables.mousedown(function () {
     draggable_actual = $(this).attr("id");
     draggable = $(this);
-    console.log(draggable_actual);
+
   })
 
-  // Compruebo antes de cambiar si es correcta la respuesta
+  /* Aplico el droppable a todos los elementos
+  Una vez aplicado el droppable utilizo drop para actuar sobre el elemento en el que coloco la foto
+  compruebo si el elemento que tengo agarrado tiene el mismo id que el droppable (id recogido en la variable droggable_actual)
+  si el id coincide cambio la imagen de fondo y aplico un visibility para el elemento draggable
+  si lo suelto y no coincide el elemento vuelve a su posicion */
   $(".droppable").droppable({
     drop: function (event, ui) {
-      droppable_actual = $(this).attr("id");
-      console.log($(this).attr("id"));
-      switch (draggable_actual) {
-        case droppable_actual:
-          $(this).css("background-image", "url(imagenes/correcto.png)");
-          draggable.css("visibility", "hidden");
-      }/*
-        if (droppable_actual == draggable_actual) {
-            alert("WASUUUUUUU");
-        }*/
 
-      /*
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "#curiosity" )
-      .html( "¡Genial, es Curiosity!" );*/
+      droppable_actual = $(this).attr("id");
+
+      if (droppable_actual == draggable_actual) {
+        $(this).css("background-image", "url(imagenes/correcto.png)");
+        draggable.css("visibility", "hidden");
+      }
     }
 
 
   });
 
+  /* Codigo para crear el tabs en el index que permite navegar por diferentes paginas,
+  le aplico collapsible: true para que cuando pulses otra vez sobre una etiqueta abierta se cierre */
   $("#tabs").tabs({
     collapsible: true
   });
 
 
-  // Estilo acordeon pasado
+  /* Codigo para el acordeon que esta localizado en la pagina de pasado
+  Cada parte del acordeon contiene un invento con informacion sobre tu su autor y el invento,
+   empezando por el mas antiguo
+   Finalmente hay botones de navegacion a la siguiente pagina y al index
+   Codigo extraido de la web de JQuery UI */
   var icons = {
     header: "ui-icon-circle-arrow-e",
     activeHeader: "ui-icon-circle-arrow-s"
   };
   $("#accordion").accordion({
     icons: icons,
-    heightStyle: "content"
+    heightStyle: "content",
+    collapsible: true
   });
-
-
   $("#toggle").button().on("click", function () {
     if ($("#accordion").accordion("option", "icons")) {
       $("#accordion").accordion("option", "icons", null);
@@ -56,9 +63,40 @@ $(function () {
     }
   });
 
-  // Estilo sortable presente
+  /* Codigo para el elemento JQuery sortable en presente al final de la pagina,
+  consiste simplemente en ordenar las palabras en el orden que tu prefieras
+  Codigo extraido de JQuery UI*/
   $("#sortable").sortable({
     placeholder: "ui-state-highlight"
   });
   $("#sortable").disableSelection();
+
+
+
+  // Codigo giro al pulsar realizado por mi
+  /*
+  $(".girar").click(function() {
+    $(".giro-futuro > .front").css("transform", "perspective( 800px) rotateY( -180deg)");
+    $(".giro-futuro > .back").css("transform", "perspective( 800px) rotateY( 0deg)");
+  });
+
+  $(".volver").click(function() {
+    $(".giro-futuro > .back").css("transform", "perspective( 800px) rotateY( 180deg)");
+    $(".giro-futuro > .front").css("transform", "perspective( 800px) rotateY( 0deg)");
+  });*/
+
+
+
+  /* Codigo facilitado por el profesor para girar la tarjeta al pulsar,
+  consiste en recoger el click del boton y guardar su contenedor padre para luego
+  aplicarle el estilo correspondiente añadiendo o quitando clase dependiendo del elemento
+  al que le des click */
+
+  $(".girar").click(function () {
+    var $parent = $(this).closest(".giro-futuro");
+
+    $parent.children(".front").toggleClass(" rotate-card-front");
+    $parent.children(".back").toggleClass(" rotate-card-back");
+  });
+
 });
