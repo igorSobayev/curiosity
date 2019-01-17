cc53a1403717a0f76b91d7d9ab26b0a9d3c6b707
igorSobayev <isobayev@gmail.com>
2019-01-17 11:01:12 +0100

    Terminada la parte del presente, añadido 8 tarjetas con robots con foto delante e informacion detras, un jquery abajo para seleccionar los mejores robots para ti, solo es decorativo porque lo mueves se coloca pero no sirve de mucho, listo para hacer futuro

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
-$( function() {
-    var draggables = $( ".draggable" ).draggable({containment: ".contenedor-juego"});
+$(function () {
+  var draggables = $(".draggable").draggable({ containment: ".contenedor-juego", revert: true });
 
-    // Guardo el valor actual del elemento
-    draggables.mousedown(function() {
-        draggable_actual = $(this).attr("id");
-        draggable = $(this);
-        console.log(draggable_actual);
-    })
+  // Guardo el valor actual del elemento
+  draggables.mousedown(function () {
+    draggable_actual = $(this).attr("id");
+    draggable = $(this);
+    console.log(draggable_actual);
+  })
 
-    // Compruebo antes de cambiar si es correcta la respuesta
-    $( ".droppable" ).droppable({
-      drop: function( event, ui ) {
-        droppable_actual = $(this).attr("id");
-        console.log($(this).attr("id"));
-        switch(draggable_actual) {
-            case droppable_actual:
-                $(this).css("background-image", "url(imagenes/correcto.png)");
-                draggable.css("visibility", "hidden");
-        }/*
+  // Compruebo antes de cambiar si es correcta la respuesta
+  $(".droppable").droppable({
+    drop: function (event, ui) {
+      droppable_actual = $(this).attr("id");
+      console.log($(this).attr("id"));
+      switch (draggable_actual) {
+        case droppable_actual:
+          $(this).css("background-image", "url(imagenes/correcto.png)");
+          draggable.css("visibility", "hidden");
+      }/*
         if (droppable_actual == draggable_actual) {
             alert("WASUUUUUUU");
         }*/
 
-        /*
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "#curiosity" )
-        .html( "¡Genial, es Curiosity!" );*/
-      }
+      /*
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "#curiosity" )
+      .html( "¡Genial, es Curiosity!" );*/
+    }
 
-      
-    });
 
-    $( "#tabs" ).tabs({
-      collapsible: true
-    });
+  });
 
-    var icons = {
-      header: "ui-icon-circle-arrow-e",
-      activeHeader: "ui-icon-circle-arrow-s"
-    };
-    $( "#accordion" ).accordion({
-      icons: icons,
-      heightStyle: "content"
-    });
-    $( "#toggle" ).button().on( "click", function() {
-      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
-        $( "#accordion" ).accordion( "option", "icons", null );
-      } else {
-        $( "#accordion" ).accordion( "option", "icons", icons );
-      }
-    });
-  } );
+  $("#tabs").tabs({
+    collapsible: true
+  });
+
+
+  // Estilo acordeon pasado
+  var icons = {
+    header: "ui-icon-circle-arrow-e",
+    activeHeader: "ui-icon-circle-arrow-s"
+  };
+  $("#accordion").accordion({
+    icons: icons,
+    heightStyle: "content"
+  });
+
+
+  $("#toggle").button().on("click", function () {
+    if ($("#accordion").accordion("option", "icons")) {
+      $("#accordion").accordion("option", "icons", null);
+    } else {
+      $("#accordion").accordion("option", "icons", icons);
+    }
+  });
+
+  // Estilo sortable presente
+  $("#sortable").sortable({
+    placeholder: "ui-state-highlight"
+  });
+  $("#sortable").disableSelection();
+});
